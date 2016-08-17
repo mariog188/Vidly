d8bc700ad82648d8d4e68eb6876b307d0bd99234
mgutierrez <mgutierrez@MGUTIERREZ-10>
2016-08-17 13:56:08 -0500

    Constraints

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -13,13 +13,15 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             Movie movie = new Movie() { Name = "Shrek" };
-            return View(movie);
+            
+            return View();
             //return Content("Hello World");
             //return HttpNotFound();
             //return new EmptyResult();
             //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "Name" });
         }
 
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content($"{year}{"/"}{month}");
