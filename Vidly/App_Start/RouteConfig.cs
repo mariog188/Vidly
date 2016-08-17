d977d115d591976b7a94cc0d4677d72802c0701b
mgutierrez <mgutierrez@MGUTIERREZ-10>
2016-08-17 07:39:33 -0500

    Custom routes

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,7 +12,14 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            //Debe ser definida desde la mas especifica a la mas generica
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { Controller = "Movie", Action = "ByReleaseDate"},
+                //new { year = @"\d{4}", month = @"\d{2}"} // Limita el año a 4 digitos y el mes a 2
+                new { year = @"2015|2016", month = @"\d{2}" } //limita el año a 2015 o 2016 
+                );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -13,11 +13,30 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             Movie movie = new Movie() { Name = "Shrek" };
-            //return View(movie);
+            return View(movie);
             //return Content("Hello World");
             //return HttpNotFound();
             //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "Name" });
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "Name" });
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content($"{year}{"/"}{month}");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content($"{"id="}{id}");
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content($"{"pageIndex="}{pageIndex}{"&sortBy="}{sortBy}");
         }
     }
 }
